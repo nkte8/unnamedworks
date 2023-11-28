257f81bc57acded9758ae8b997d23abfb6f499fb
nkte8 <nkte8@outlook.jp>
2023-11-28 22:56:51 +0900

    スクリプトの修正

--- convertimgs.sh
+++ convertimgs.sh
@@ -7,7 +7,7 @@ apt list --installed 2>/dev/null | grep pngquant >/dev/null
 apt list --installed 2>/dev/null | grep convert >/dev/null
 [[ $? -ne 0 ]] && sudo apt install imagemagick -y
 
-GIT_UNCOMPRESSED_FILES=$(git status | grep .png | grep -v "public" | xargs)
+GIT_UNCOMPRESSED_FILES=$(git status | grep .png | xargs)
 echo "compless... $GIT_UNCOMPRESSED_FILES"
 pngquant --ext .png --force $GIT_UNCOMPRESSED_FILES
 
