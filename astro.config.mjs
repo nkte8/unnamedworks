52bd44b9dccd433143068a003a29ab5e117d3c1b
nkte8 <nkte8@outlook.jp>
2023-12-02 01:41:43 +0900

    トランジションを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,9 +14,5 @@ export default defineConfig({
   },
   integrations: [
     partytown(),
-    swup({
-      theme: 'false',
-      smoothScrolling: true,
-      updateBodyClass: true,
-    })]
+  ]
 });
