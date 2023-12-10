3b8fc41859623c05ae53859b70fa69c6a6746635
nkte8 <nkte8@outlook.jp>
2023-12-10 09:24:30 +0000

    OGPカードを自動で生成する仕組みを作成

--- src/utils/newt.ts
+++ src/utils/newt.ts
@@ -25,18 +25,32 @@ export const getAllPosts = async () => {
 
 export const getLatestHighlight = async () => {
     const allPosts = client
-    .getContents({
-        appUid: import.meta.env.NEWT_HIGHLIGHTS_APP_UID,
-        modelUid: import.meta.env.NEWT_HIGHLIGHTS_MODEL_UID,
-        query: {
-            limit: 1,
-            order: ['-pubDate'],
-        }
-    })
-    .then((content) => content)
-    .catch((err) => {
-        console.log(err);
-        return err;
-    });
+        .getContents({
+            appUid: import.meta.env.NEWT_HIGHLIGHTS_APP_UID,
+            modelUid: import.meta.env.NEWT_HIGHLIGHTS_MODEL_UID,
+            query: {
+                limit: 1,
+                order: ['-pubDate'],
+            }
+        })
+        .then((content) => content)
+        .catch((err) => {
+            console.log(err);
+            return err;
+        });
     return allPosts;
-  }
+}
+
+export function create_ogp_urlquery(width: number, height: number, focus_bottom: boolean):string {
+    // ogpカードは変換により開けなくなる可能性があるため、formatを行わない。
+    var assert_api_function:string = "?width=600"
+    var ogp_width = width - 1
+    var ogp_height = Math.floor(height / 2)
+    var ogp_top = Math.floor(height / 8)
+    if (Boolean(focus_bottom) == true){
+        ogp_top = ogp_top * 3
+    }
+    assert_api_function += 
+        "&extract=1," + ogp_top + "," + ogp_width  + "," + ogp_height 
+    return assert_api_function
+}
