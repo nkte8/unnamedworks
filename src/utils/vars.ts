2b3d7aed36740b8f24c0d8486816436cbb6eb04f
nkte8 <nkte8@outlook.jp>
2023-12-29 21:42:16 +0000

    表示メッセージの追加。リダイレクトの設定実施

--- src/utils/vars.ts
+++ src/utils/vars.ts
@@ -2,3 +2,4 @@ export const lambdaurl = import.meta.env.LAMBDA_URL;
 export const baseurl = "/post/";
 export const website_title = "ㅤ"
 export const indexpage_slug = "index"
+export const login_page_path = "/secret"
